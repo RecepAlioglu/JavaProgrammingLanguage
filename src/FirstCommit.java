151da38fe0fdc3b2aeb262824270189f76f07791
Timberman <timberman@Timbermans-MBP.fritz.box>
2022-02-23 16:27:24 +0100

    SecondCommit

--- src/FirstCommit.java
+++ src/FirstCommit.java
@@ -1,5 +1,5 @@
 public class FirstCommit {
     public static void main(String[] args) {
-        System.out.println("I am Recep and the first one who came here");
+        System.out.println("I am Recep and and Created my own branch");
     }
 }
